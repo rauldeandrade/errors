ea033e8e0d189adc637e94f82338e503ccead56a
Raul De Andrade <69761277+rauldeandrade@users.noreply.github.com>
2022-07-04 13:07:09 +0100

    Create repoErr.go

---
+++ repoErr.go
@@ -0,0 +1,38 @@
+package errors
+
+type RepoError struct {
+	BaseError
+}
+
+// New Service Error
+func NewRepoError(be *BaseError) *ServiceError {
+	return &ServiceError{
+		BaseError: *be,
+	}
+}
+
+// // NEED BETTER NAMES
+// func NewServiceErrorWithRoot(kind, rootCause string) *ServiceError {
+// 	return &ServiceError{
+// 		BaseError: *BaseWithRoot(kind, rootCause),
+// 	}
+// }
+
+// func (ce *ServiceError) JsonRes() map[string]interface{} {
+
+// 	ce.jsonResError = map[string]interface{}{"error": ce.Error()}
+// 	return ce.jsonResError
+// }
+
+// func (ce *ServiceError) GetHttpCode() int {
+// 	switch ce.BaseError.kind {
+// 	//# I want to change this
+// 	case "not found":
+// 		ce.httpCode = 404
+// 	case "bad request":
+// 		ce.httpCode = 400
+// 	default:
+// 		ce.httpCode = 500
+// 	}
+// 	return ce.httpCode
+// }
